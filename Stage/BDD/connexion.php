2b0d242a20ca06d4bd7de65a258f5f2e69bbf5b7
TomAllusse <107853733+TomAllusse@users.noreply.github.com>
2023-06-01 14:31:33 +0200

    Rendu 5 jour 3

---
+++ Stage/BDD/connexion.php
@@ -0,0 +1,9 @@
+<?php
+    require_once('getBDD.php');
+    $bdd = connexionBDD();
+    $OK = false;
+    if(){
+        $bdd = null
+        $OK = true;
+    }
+?>
---
+++ Stage/BDD/connexionBDD.php
@@ -0,0 +1,9 @@
+<?php
+// Fonction qui effectue la connexion à la BDD
+// Elle renvoie un objet de la classe PDO par le biais de son constructeur
+function connexionBDD() {
+    // Connexion à la BD
+    $bdd = new PDO("mysql:host=localhost; dbname=desert;charset=utf8", "root", "");
+    return $bdd;
+}
+?>
