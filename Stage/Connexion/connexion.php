3ba486c232e5d882a315f4942c86f0a3710f3009
TomAllusse <107853733+TomAllusse@users.noreply.github.com>
2023-06-02 11:50:07 +0200

    R2J4

--- Stage/Connexion/connexion.php
+++ Stage/Connexion/connexion.php
@@ -6,52 +6,33 @@
         // Récupération des champs saisis
         $user = $_POST['identifiant'];
         $password = $_POST['password'];
-        $req = "SELECT * FROM `users` where E_mail=\"$user\"";
-        $prep = $bdd->prepare($req);
+        $prep = $bdd->prepare("SELECT * FROM `users` where E_mail=:user");
+        $prep->bindValue(":user", $user);
         $prep->execute();
         $rsuser= $prep->fetch();
 
         if($rsuser){
             $pass = password_verify($password, $rsuser["Passwords"]);
-            $pwd = $rsuser["Passwords"];
             if($pass){
-                echo "ok 2";
-                $requete = "SELECT * FROM `users` where E_mail=\"$user\" and Passwords=\"$pwd\"";
-                $prep = $bdd->prepare($requete);
+                $prep = $bdd->prepare("SELECT * FROM `users` where E_mail=:user and Passwords=:passwords");
+                $prep->bindValue(":user", $user);
+                $prep->bindValue(":passwords", $rsuser["Passwords"]);
                 $prep->execute();
                 $rsid= $prep->fetchAll();
-                foreach($rsid as $resultat+){
-                    echo "<p>Nom : $resultat[Name_User] / Prenom : $resultat[FirstName] / Role : $resultat[Roles]";
+                foreach($rsid as $resultat){
+                    echo "<p style=\"padding:1000px\">Nom : ".$resultat["Name_User"]." / Prenom : ".$resultat["FirstName"]." / Role : ".$resultat["Roles"].".</p>";
                 }    
             }else{
-                var_dump($pass);
                 echo "Identifiant ou mots de passe invalide";
             }
         }
 
-        $requete = "SELECT * FROM `users` where E_mail=\"$user\" and Passwords=\"$pass\"";
-        $prep = $bdd->prepare($requete);
+        /*$prep = $bdd->prepare("SELECT * FROM `users` where E_mail=\"$user\" and Passwords=\"$pass\"");
         $prep->execute();
         $rsid= $prep->fetchAll();
-        $nbr=$prep->rowCount();
+        $nbr=$prep->rowCount();*/
         // Fermer la connexion à la base de données
         $bdd = null;
         $OK = true; // Variable drapeau indiquant le succès de l'ajout
     }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Connexion</title>
-</head>
-<body>
-    <?php 
-        //header('Location: ..\admin.php');
-
-    ?>
-</body>
-</html>
