521e6805cdc772c6d93d4d16e3d238c03cb28f91
TomAllusse <107853733+TomAllusse@users.noreply.github.com>
2023-06-06 11:56:44 +0200

    R4J2S2

--- Stage/admin.php
+++ Stage/admin.php
@@ -1,4 +1,9 @@
 <?php
+
+    require_once 'BDD/connexionBDD.php';
+
+    $bdd = connexionBDD();    
+
     session_start();
 
     $role = $_SESSION['user']['role'];
@@ -23,43 +28,76 @@
 <body id="corps">
     <?php
         require_once('layouts/nav-bar-admin.php');
-    ?>   
+    ?>
     <main>
         <table id="Tab1">
             <thead>
                 <tr>
-                    <th>Tous les utilisateurs</th>
+                    <th colspan="8">Tous les utilisateurs</th>
                 </tr>
             </thead>    
             <tbody>
                 <tr>
-                    <?php
-                        $prep = $bdd->prepare("SELECT * FROM `users`");
-                        $prep->execute();
-                        $users= $prep->fetchall();
-                        foreach($users as $user){
-                            echo '
-                            <td>' .$user['FirstName']. '</td>
-                            <td>' .$user['Name_User']. '</td>
-                            <td>' .$user['Date_Of_Birth'].  '</td>
-                            <td>' .$user['E_mail']. '</td>
-                            <td>' .$user['Phone'].  '</td>
-                            <td>' .$user['Passwords'].  '</td>
-                            <td>' .$user['Roles'].  '</td>
-                            <td>' .$user['Picture_User'].  '</td>';
-                        }
-                    ?>
+                    <td class="AffichageUser">FirstName</td>
+                    <td class="AffichageUser">Name</td>
+                    <td class="AffichageUser">Date Of Birth</td>
+                    <td class="AffichageUser">Email</td>
+                    <td class="AffichageUser">Phone</td>
+                    <td class="AffichageUser">Passwords</td>
+                    <td class="AffichageUser">Roles</td>
+                    <td class="AffichageUser">Picture</td>
                 </tr>
+                <?php
+                    $prep = $bdd->prepare("SELECT * FROM `users`");
+                    $prep->execute();
+                    $users= $prep->fetchall();
+                    foreach($users as $user){
+                        echo '
+                        <tr>
+                            <td class="AffichageUser">' .$user['FirstName']. '</td>
+                            <td class="AffichageUser">' .$user['Name_User']. '</td>
+                            <td class="AffichageUser">' .$user['Date_Of_Birth'].  '</td>
+                            <td class="AffichageUser">' .$user['E_mail']. '</td>
+                            <td class="AffichageUser">' .$user['Phone'].  '</td>
+                            <td class="AffichageUser">' .$user['Passwords'].  '</td>
+                            <td class="AffichageUser">' .$user['Roles'].  '</td>
+                            <td class="AffichageUser">' .$user['Picture_User'].  '</td>
+                        </tr>';
+                    }
+                 ?>
             </tbody>
         </table>  
         <table id="Tab2">
             <thead>
                 <tr>
-                    <th>Article liées aux utilisateurs</th>
+                    <th  colspan="6">Article liées aux utilisateurs</th>
                 </tr>
             </thead>   
             <tbody>
-                
+                <tr>
+                    <td class="AffichageUser">FirstName</td>
+                    <td class="AffichageUser">Name</td>
+                    <td class="AffichageUser">Title</td>
+                    <td class="AffichageUser">Picture</td>
+                    <td class="AffichageUser">Contained</td>
+                    <td class="AffichageUser">Created at</td>
+                </tr>
+                <?php
+                    $prep = $bdd->prepare("SELECT * FROM `users` u INNER JOIN `post` p WHERE u.Id_Post=p.Id_Post;");
+                    $prep->execute();
+                    $users= $prep->fetchall();
+                    foreach($users as $user){
+                        echo '
+                        <tr>
+                            <td class="AffichageUser">' .$user['FirstName']. '</td>
+                            <td class="AffichageUser">' .$user['Name_User']. '</td>
+                            <td class="AffichageUser">' .$user['Title'].  '</td>
+                            <td class="AffichageUser">' .$user['Picture'].  '</td>
+                            <td class="AffichageUser">' .$user['Contained'].  '</td>
+                            <td class="AffichageUser">' .$user['Created_at'].  '</td>
+                        </tr>';
+                    }
+                 ?>
             </tbody>
         </table>   
     </main>
