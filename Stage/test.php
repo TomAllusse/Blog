ed08e4f11183c64744d66827c96f397e07875a2a
TomAllusse <107853733+TomAllusse@users.noreply.github.com>
2023-06-08 10:06:52 +0200

    R1J4S2
    Bouton admin + informations de l'utilisateur page test

--- Stage/test.php
+++ Stage/test.php
@@ -50,49 +50,42 @@
             </thead>    
             <tbody>
                 <?php
-                    if (isset($_POST['button'])) {
-                        echo $_POST['button'];
+
+                    function before() {
+                        $_SESSION['max'] = $_SESSION['max'] - 10;
+                        $_SESSION['min'] = $_SESSION['min'] - 10;
                     }
+                    function after() {
+                        $_SESSION['max'] = $_SESSION['max'] + 10;
+                        $_SESSION['min'] = $_SESSION['min'] + 10;
+                    }
+
                     $prep = $bdd->prepare("SELECT MAX(Id_User) FROM `users`");
                     $prep->execute();
                     $id= $prep->fetchColumn();
-                    if(!empty($_SESSION['max'])){
+                    
+                    if(isset($_GET['before'])){
+                        before();
+                    }else if (isset($_GET['after'])){
+                        after();
+                    }
+                    
+                    if(empty($_SESSION['max'])){
+                        $_SESSION['max'] = 10;
+                        $_SESSION['min'] = 0;
+                    }
+                    
+                    if($_SESSION['max'] <=0 || $_SESSION['max'] >= $id && ($id % 10) == 0 || $_SESSION['max'] >= ($id + 10)) {
                         $_SESSION['max'] = 10;
                         $_SESSION['min'] = 0;
                         $max = $_SESSION['max'];
                         $min = $_SESSION['min'];
-                    }if(isset($_SESSION['Button']) && $_SESSION['Button'] == 'before'){
-                        $_SESSION['max'] = $_SESSION['max'] - 10;
-                        $_SESSION['min'] = $_SESSION['min'] - 10;
-                        $max = $_SESSION['max'];
-                        $min = $_SESSION['min'];
-                        if($_SESSION['max'] <=0) {
-                            $_SESSION['max'] = 10;
-                            $_SESSION['min'] = 0;
-                            $max = $_SESSION['max'];
-                            $min = $_SESSION['min'];
-                        }
-                    }else if(isset($_SESSION['Button']) && $_SESSION['Button'] == 'after'){
-                        echo $_SESSION['max'].'-'. $_SESSION['min'].'<br>';
-                        $_SESSION['max'] = $_SESSION['max'] + 10;
-                        $_SESSION['min'] = $_SESSION['min'] + 10;
-                        $max = $_SESSION['max'];
-                        $min = $_SESSION['min'];
-                        echo $_SESSION['max'].'-'. $_SESSION['min'].'<br>';
-                        var_dump($_SESSION['max'] >= $id, $id%10 == 0, $_SESSION['max'] >= $id+10);
-                        /*if($_SESSION['max'] >= $id && $id%10 == 0 || $_SESSION['max'] >= $id+10) {
-                            $_SESSION['max'] = 10;
-                            $_SESSION['min'] = 0;
-                            $max = $_SESSION['max'];
-                            $min = $_SESSION['min'];
-                        }*/
                     }
-
+                    $max = $_SESSION['max'];
+                    $min = $_SESSION['min'];
                     if($max <= $id){
-                        echo $max.'-x-'. $min.'<br>';
                         $prep = $bdd->prepare("SELECT * FROM `users` WHERE `Id_User`> $min AND `Id_User`<= $max");
                     }else{
-                        echo $id.'x'. $min.'<br>';
                         $prep = $bdd->prepare("SELECT * FROM `users` WHERE `Id_User`> $min AND `Id_User`<= $id");
                     }
                     $prep->execute();
@@ -113,25 +106,22 @@
                  ?>
             </tbody>
         </table>
-        <form action="admin.php" method="post">
-            <div id="button2"> 
-                <button class="svg-button" type="submit" name="button" value="before"></button>
-                    <svg class="arrow-svg" width="100px" height="100px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <g>
-                            <path class="arrow" d="M12.25 8.25C10.6879 9.8121 9.8121 10.6879 8.25 12.25L12.25 16.25" stroke="#F6F1F1" stroke-width="1.00088" />
-                        </g>
-                    </svg>
-                </button>
-            </div>
-            <div id="button2"> 
-                <button class="svg-button" type="submit" name="button" value="after"></button>
-                    <svg class="arrow2-svg"width="100px" height="100px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <g>
-                            <path class="arrow2" d="M11.75 8.25C13.3121 9.8121 14.1879 10.6879 15.75 12.25L11.75 16.25" stroke="#F6F1F1" stroke-width="1.00088"/>
-                        </g>
-                    </svg>
-            </div>
-        </form>
+        <div id="button2"> 
+            <a href="test.php?before=true">
+                <svg width="100px" height="100px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g>
+                        <path class="arrow" d="M12.25 8.25C10.6879 9.8121 9.8121 10.6879 8.25 12.25L12.25 16.25" stroke="#F6F1F1" stroke-width="1.00088" />
+                    </g>
+                </svg>
+            </a>
+            <a href="test.php?after=true">
+                <svg width="100px" height="100px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g>
+                        <path class="arrow2" d="M11.75 8.25C13.3121 9.8121 14.1879 10.6879 15.75 12.25L11.75 16.25" stroke="#F6F1F1" stroke-width="1.00088"/>
+                    </g>
+                </svg>
+            </a>
+        </div>
     </main>
     <?php
         require_once('layouts/footer.php');
@@ -139,22 +129,6 @@
 
 </body>
 <script src="js/javascript.js"></script>
-<script>
-    document.addEventListener('DOMContentLoaded', function() {
-        var svgButtons = document.getElementsByClassName('svg-button');
-        
-        for (var i = 0; i < svgButtons.length; i++) {
-            svgButtons[i].addEventListener('click', handleButtonClick);
-        }
-        
-        function handleButtonClick(event) {
-            // Votre code pour gérer le clic sur le bouton SVG ici
-            event.preventDefault(); // Empêche le formulaire de se soumettre si nécessaire
-            console.log('Bouton SVG cliqué');
-            // Effectuez les actions souhaitées ici
-        }
-    });
-</script>
 </html>
 
 <!--<div>
